ef54c43d13923b0ccaecb8734e978068a7147a29
ccoley <chris.num2@gmail.com>
2014-07-08 15:40:04 -0700

    optimization

--- cycle.js
+++ cycle.js
@@ -1,6 +1,15 @@
-// Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-    // No tabs or host permissions needed!
-    console.log('Turning ' + tab.url + ' red!');
-    chrome.tabs.executeScript(null, {file: "content.js"});
+    var url = tab.url;
+    var start = url.indexOf('//') + 2;
+    var dsdev = url.indexOf('.ds.dev', start);
+    var middle = url.indexOf(':8080', start);
+    var end = url.indexOf('/', start);
+
+    if (dsdev != -1) {          // On local, goto staging
+        chrome.tabs.update(tab.id, {url: 'http://' + url.substring(start, dsdev) + ':8080' + url.substring(end)});
+    } else if (middle != -1) {  // On staging, goto live
+        chrome.tabs.update(tab.id, {url: 'http://' + url.substring(start, middle) + url.substring(end)});
+    } else {                    // On live, goto local
+        chrome.tabs.update(tab.id, {url: 'http://' + url.substring(start, end) + '.ds.dev' + url.substring(end)});
+    }   
 });
